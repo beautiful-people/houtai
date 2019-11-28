a56ba069eedc04122b46c6ed0cdb51e1f08d884f
LEIYING29 <leiying44@163.com>
2019-11-28 15:03:11 +0800

    各个组件的初始创建

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,66 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="about">
+    <div class="left">
+      <el-row>
+        <el-col :span="14">
+          <el-menu
+            default-active="allusers"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+            unique-opened
+            router
+          >
+            <el-menu-item index="BusinessManagement">商家管理</el-menu-item>
+            <el-menu-item index="manageusergroup">管理用户</el-menu-item>
+            <el-menu-item index="TheTenderReview">招标审核</el-menu-item>
+            <el-menu-item index="imageUpload">图片上传</el-menu-item>
+            <el-menu-item index="peakfire">商家认证</el-menu-item>
+          </el-menu>
+        </el-col>
+      </el-row>
     </div>
-    <router-view/>
+    <div class="right">
+      <router-view />
+    </div>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
     }
   }
+};
+</script>
+
+<style lang="less" scoped>
+.about {
+  overflow: hidden;
+}
+.left {
+  width: 200px;
+  height: 100%;
+  float: left;
+}
+.right {
+  float: left;
+  padding: 20px 40px;
+}
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  height: 600px;
+  min-height: 400px;
+}
+.el-row {
+    text-align: center;
 }
 </style>
---
+++ src/components/TheTenderReview.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="TheTenderReview"></div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
